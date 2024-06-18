32fac2bdde569dfbf6f19562b4265d2b59c72abb
Mike Petrichenko <btframework@gmail.com>
2024-06-18 18:38:05 +0300

    Added Lazarus package files

---
+++ Packages/wclbluetoothframework.pas
@@ -0,0 +1,17 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit wclBluetoothFramework;
+
+{$warn 5023 off : no warning about unused units}
+interface
+
+uses
+  wclAudio, wclAudioApi, wclAudioErrors, wclBluetooth, wclBluetoothBled112, 
+  wclBluetoothBlueSoleil, wclBluetoothErrors, wclBluetoothMessages, 
+  wclBluetoothMicrosoft, wclBluetoothToshiba, wclCODs, wclUUIDs;
+
+implementation
+
+end.
---
+++ Packages/wclcommon.pas
@@ -0,0 +1,16 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit wclCommon;
+
+{$warn 5023 off : no warning about unused units}
+interface
+
+uses
+  wclErrors, wclHelpers, wclMessaging, wclPowerEvents, wclThread, wclWinAPI, 
+  wclWinUsb, msxml;
+
+implementation
+
+end.
